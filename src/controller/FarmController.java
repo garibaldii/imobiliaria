73f0a9322059ab942865c146679a7152f4b9a5e6
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-17 14:31:00 -0300

    feat: initializing singleton farm Controller

--- src/controller/FarmController.java
+++ src/controller/FarmController.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class FarmController {
 
-    private static FarmController instance;
+    private static FarmController instance = new FarmController();
 
     public static FarmController getInstance() {
         return instance;
