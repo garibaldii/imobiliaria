dc829ef039f9b2e94c4868059f071db35ae2ebc7
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-17 15:34:50 -0300

    feat: add domesticHouse controller

---
+++ src/controller/DomesticHouseController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.residence.DomesticHouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DomesticHouseController {
+
+    List<DomesticHouse> availableHouses = new ArrayList<>();
+
+    private static DomesticHouseController instance = new DomesticHouseController();
+
+    public static DomesticHouseController getInstance() {
+        return instance;
+    }
+
+    public List<DomesticHouse> getAvailableHouses() {
+        System.out.println(availableHouses);
+        return availableHouses;
+    }
+
+
+    public DomesticHouse postDomesticHouse(DomesticHouse h) {
+        availableHouses.add(h);
+        return h;
+    }
+}
