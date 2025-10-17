e9ad36223c7349a10da0ca501f6d68a8214a8c0a
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-17 12:34:22 -0300

    refactor: override extends methods

--- src/model/residence/Farm.java
+++ src/model/residence/Farm.java
@@ -8,7 +8,22 @@ public class Farm extends Residence {
     private FarmType type;
 
 
-    public Farm(String postalCode, int number, Owner owner, boolean rented, int rentPrice) {
+    @Override
+    public int getBrokerFee() {
+        return (int) (getRentPrice() * 1.05);
+    }
+
+    public Farm(String postalCode, int number, Owner owner, boolean rented, int rentPrice, FarmType type) {
         super(postalCode, number, owner, rented, rentPrice);
+        this.type = type;
+    }
+
+    @Override
+    public String getType() {
+        return type.getLabel();
+    }
+
+    public void setType(FarmType type) {
+        this.type = type;
     }
 }
